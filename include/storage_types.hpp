d387b1a4083378771e8ac1d8c9db7e14d36c91ca
Anna Wojcik <annawoj@student.agh.edu.pl>
2023-12-06 09:09:00 +0100

    implementacja IPackageQueue i PackageQueue

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -30,12 +30,34 @@ public:
     virtual ~IPackageStockpile() = default;
 };
 
-class IPackageQueue : public IPackageStockpile{
-    throw;
+class IPackageQueue : public IPackageStockpile {
+public:
+    virtual Package pop() = 0;
+    virtual PackageQueueType get_queue_type() const = 0;
+
+    ~IPackageQueue() override = default;
 };
 
-class PackageQueue{
-    throw;
+class PackageQueue : public IPackageQueue {
+public:
+    explicit PackageQueue(PackageQueueType queue_type) : queue_(), queue_type_(queue_type) {}
+
+    const_iterator begin() const override {return queue_.cbegin();}
+    const_iterator cbegin() const override {return queue_.cbegin();}
+    const_iterator end() const override {return queue_.cend();}
+    const_iterator cend() const override {return queue_.cend();}
+
+    void push(Package&& package) override {queue_.emplace_back(std::move(package));}
+    std::size_t size() const override {return queue_.size();}
+    bool empty() const override {return queue_.empty();}
+    Package pop() override;
+    PackageQueueType get_queue_type() const override {return queue_type_;}
+
+    ~PackageQueue() override = default;
+
+private:
+    std::list<Package> queue_;
+    PackageQueueType queue_type_;
 };
 
 #endif //SYMULACJASIECIZPO_STORAGE_TYPES_HPP
