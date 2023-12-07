4e1e52fe5374b8b1768c750a417230c55a5eb0e1
Anna Wojcik <annawoj@student.agh.edu.pl>
2023-12-07 11:54:19 +0100

    implementacja metody pop

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,3 +1,19 @@
 //
 // Created by julia on 30.11.2023.
 //
+
+# include "storage_types.hpp"
+
+Package PackageQueue::pop() {
+    Package package;
+
+    if (queue_type_ == PackageQueueType::FIFO) {
+        package = std::move(queue_.front());
+        queue_.pop_front();
+    }
+    else if (queue_type_ == PackageQueueType::LIFO) {
+        package = std::move(queue_.back());
+        queue_.pop_back();
+    }
+    return package;
+}
