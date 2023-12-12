c96bcbfee11a5dd23e7a31c1b3108a7791c7495c
Julia Wiater <juliaw@student.agh.edu.pl>
2023-12-12 18:44:23 +0100

    aliasy

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -5,4 +5,6 @@
 #ifndef SYMULACJASIECIZPO_NODES_HPP
 #define SYMULACJASIECIZPO_NODES_HPP
 
+//definicje klas IPackageReceiver, Storehouse, ReceiverPreferences, PackageSender, Ramp i Worker - do 15 grudnia
+
 #endif //SYMULACJASIECIZPO_NODES_HPP
--- include/types.hpp
+++ include/types.hpp
@@ -5,8 +5,14 @@
 #ifndef SYMULACJASIECIZPO_TYPES_HPP
 #define SYMULACJASIECIZPO_TYPES_HPP
 
-//definicja aliasu ElementID - do 8 grudnia (done)
+#include <functional>
 
-using ElementID = int;
+//definicja aliasu ElementID - do 8 grudnia (done)
+//definicja aliasów Time, TimeOffset i ProbabilityGenerator - do 15 grudnia (done)
+
+using ElementID = unsigned int;
+using Time = int;
+using TimeOffset = int;
+using ProbabilityGenerator = std::function<double()>;
 
 #endif //SYMULACJASIECIZPO_TYPES_HPP
