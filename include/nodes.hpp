5c5a2a57f1892b30ff7ee9d587a2f4f260f24c1c
Anna Wojcik <annawoj@student.agh.edu.pl>
2023-12-14 09:55:01 +0100

    implementacja ramp i package sender

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -5,4 +5,34 @@
 #ifndef SYMULACJASIECIZPO_NODES_HPP
 #define SYMULACJASIECIZPO_NODES_HPP
 
+
+class PackageSender {
+public:
+    ReceiverPreferences receiver_preferences_;
+    PackageSender() = default;
+    PackageSender(PackageSender &&pack_sender) = default;
+    void send_package();
+    const std::optional<Package> &get_sending_buffer() const {return bufor_;}
+
+protected:
+    void push_package(Package &&package) {bufor_.emplace(package.get_id());};
+
+private:
+    std::optional<Package> bufor_ = std::nullopt;
+};
+
+class Ramp : public PackageSender {
+public:
+    Ramp(ElementID id, TimeOffset di) : PackageSender(), id_(id), di_(di) {}
+    void deliver_goods(Time t);
+    TimeOffset get_delivery_interval() const {return di_;}
+    ElementID get_id() const {return id_;}
+
+private:
+    ElementID id_;
+    TimeOffset di_;
+    Time t_;
+    std::optional<Package> bufor_ = std::nullopt;
+};
+
 #endif //SYMULACJASIECIZPO_NODES_HPP
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,3 +1,27 @@
 //
 // Created by julia on 30.11.2023.
 //
+
+#include "nodes.hpp"
+
+void PackageSender::send_package() {
+    IPackageReceiver *receiver;
+    if (bufor_) {
+        receiver = receiver_preferences_.choose_receiver();
+        receiver->receive_package(std::move(*bufor_));
+        bufor_.reset();
+    }
+}
+
+void Ramp::deliver_goods(Time t) {
+    if (!bufor_) {
+        push_package(Package());
+        bufor_.emplace(id_);
+        t_ = t;
+    }
+    else {
+        if (t - di_ == t_) {
+            push_package(Package());
+        }
+    }
+}
