1001ccc3e229305b99f7324bd46254c2ef928bb9
Venom7312 <80624475+Venom7312@users.noreply.github.com>
2023-12-15 20:19:44 +0100

    Update helpers.hpp

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -4,7 +4,17 @@
 
 #ifndef SYMULACJASIECIZPO_HELPERS_HPP
 #define SYMULACJASIECIZPO_HELPERS_HPP
+#include <functional>
+#include <random>
 
+#include "types.hpp"
+
+extern std::random_device rd;
+extern std::mt19937 rng;
+
+extern double default_probability_generator();
+
+extern ProbabilityGenerator probability_generator;
 //ewentualne pomocnicze obiekty i funkcje globalne (np. generator liczb losowych)
 
 #endif //SYMULACJASIECIZPO_HELPERS_HPP
