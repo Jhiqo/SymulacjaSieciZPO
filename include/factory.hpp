107174af209ed1c4341525c4ce9487c8e200b852
Julia Wiater <juliaw@student.agh.edu.pl>
2023-12-20 14:46:42 +0100

    node collection

--- include/factory.hpp
+++ include/factory.hpp
@@ -1,8 +1,28 @@
-//
-// Created by julia on 30.11.2023.
-//
-
 #ifndef SYMULACJASIECIZPO_FACTORY_HPP
 #define SYMULACJASIECIZPO_FACTORY_HPP
+
+#include "types.hpp"
+#include "nodes.hpp"
+#include <vector>
+
+template <class Node>
+class NodeCollection{
+public:
+    using container_t = typename std::list<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
+
+    void add_node(Node&& node) {nodes_.push_back(std::move(node));}
+    void remove_by_id(ElementID id);
+    NodeCollection<Node>::iterator find_by_id(ElementID id) {return std::find_if(nodes_.begin(), nodes_.end(), [id](Node& elem) {return elem.get_id()==id;});}
+    NodeCollection<Node>::const_iterator find_by_id(ElementID id) const {return std::find_if(nodes_.cbegin(), nodes_.cend(), [id] (const Node& elem) {return elem.get_id()==id;});}
+
+    iterator begin() {return nodes_.begin();}
+    iterator end() {return nodes_.end();}
+    const_iterator cbegin() const {return nodes_.cbegin();}
+    const_iterator cend() const {return nodes_.cend();}
+private:
+    container_t nodes_;
+};
 
 #endif //SYMULACJASIECIZPO_FACTORY_HPP
--- src/factory.cpp
+++ src/factory.cpp
@@ -1,3 +1,9 @@
-//
-// Created by julia on 30.11.2023.
-//
+#include "factory.hpp"
+
+template<class Node>
+void NodeCollection<Node>::remove_by_id(ElementID id) {
+    auto it = find_by_id(id);
+    if (it != nodes_.end()){
+        nodes_.erase(it);
+    }
+}
