2c8d048983619f14d50f12a4193688f654ff49d2
Anna Wojcik <annawoj@student.agh.edu.pl>
2023-12-29 12:03:52 +0100

    uzupelniona symulacja

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,3 +1,33 @@
 //
 // Created by julia on 30.11.2023.
 //
+
+#include "simulation.hpp"
+#include "reports.hpp"
+#include <stdexcept>
+
+SpecificTurnsReportNotifier::should_generate_report(Time t) {
+	return turns_.find(t) != turns_.end();
+}
+
+IntervalReportNotifier::should_generate_report(Time t) {
+	return (t - 1) % to_ == 0;
+}
+
+void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)> r) {
+	if (!f.isconsistent()) {
+		throw std::logic_error("Inconsistent graph");
+	}
+
+	Time t = 1;
+
+	while (t <= d) {
+
+		f.do_delivery(t);
+		f.do_package_passing();
+		f.do_work(t);
+		r(f, t);
+
+		t++;
+	}
+}
