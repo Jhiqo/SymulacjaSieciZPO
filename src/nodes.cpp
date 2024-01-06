80ecbcc513d3e71ab2fee51b2c00bb379a5aaf70
Anna Wojcik <annawoj@student.agh.edu.pl>
2024-01-06 18:22:10 +0100

    zmienione deliver goods

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -29,21 +29,14 @@ void PackageSender::send_package() {
     IPackageReceiver *receiver;
     if (bufor_) {
         receiver = receiver_preferences_.choose_receiver();
-        receiver->receive_package(std::move(*bufor_));
+        receiver->receive_package(std::move(bufor_.value()));
         bufor_.reset();
     }
 }
 
 void Ramp::deliver_goods(Time t) {
-    if (!bufor_) {
+    if (t % di_ == 1) {
         push_package(Package());
-        bufor_.emplace(id_);
-        t_ = t;
-    }
-    else {
-        if (t - di_ == t_) {
-            push_package(Package());
-        }
     }
 }
 
